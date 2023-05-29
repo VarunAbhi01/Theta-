f46d08cbbc9f1a92de18a6746ac327578d3a8c0b
VarunAbhi01 <VarunAbhi321@gmail.com>
2023-05-29 11:15:22 +0530

    signup page added

---
+++ signup page.js
@@ -0,0 +1,113 @@
+import React from "react";
+import "./style.css";
+
+export const SignupPage = () => {
+return (
+    <div className="signup-page">
+    <div className="div">
+      <div className="overlap">
+        <h1 className="LINKED">
+          <span className="text-wrapper">LINKED</span>
+        </h1>
+        <div className="overlap-group3">
+        <div className="rectangle" />
+          <div className="in">
+            <span className="span">in</span>
+          </div>
+        </div>
+      </div>
+      <div className="overlap-group1">
+        <div className="about">
+          <span className="text-wrapper-2">About</span>
+        </div>
+<div className="accessibility">
+    <span className="text-wrapper-2">Accessibility</span>
+</div>
+<div className="user-agreement">
+    <span className="text-wrapper-2">User Agreement</span>
+</div>
+<div className="privacy-policy">
+    <span className="text-wrapper-2">Privacy policy</span>
+</div>
+<div className="cookie-policy">
+    <span className="text-wrapper-2">Cookie policy</span>
+</div>
+<div className="copyright-policy">
+    <span className="text-wrapper-2">Copyright Policy</span>
+</div>
+<div className="brand-policy">
+    <span className="text-wrapper-2">Brand Policy</span>
+</div>
+<div className="guest-controls">
+  <span className="text-wrapper-2">Guest Controls</span>
+</div>
+<div className="community-guidelines">
+  <span className="text-wrapper-2">Community Guidelines</span>
+</div>
+<div className="language">
+  <span className="text-wrapper-2">Language</span>
+  </div> 
+  <img className="image" alt={"Image"} src={"image-1-2.png"} />
+</div>
+<div className="overlap-group4">
+  <div className="span-wrapper">
+    <span className="text-wrapper-3">LINKED</span>
+  </div>
+  <div className="rectangle-8" />
+  <div className="in-2">
+    <span className="text-wrapper-4">in</span>
+  </div>
+</div>
+</div>
+<p className="make-the-most-of-your-professional-life">
+  <span className="text-wrapper-5">Make the most of your professional life</span>
+</p>
+        <div className="overlap-group">
+          <div className="email-or-phone-number">
+            <span className="text-wrapper-6">Email or phone number</span>
+          </div>
+          <div className="password-or-more-characters">
+            <span className="text-wrapper-6">Password (6 or more characters)</span>
+          </div>
+          <div className="rec-email" />
+          <div className="rec-password" />
+          <div className="overlap-groups">
+            <div className="agree-join">
+              <span className="text-wrapper-7">Agree &amp; Join</span>
+            </div>
+            </div>
+          <div className="overlap-group6">
+            <img className="google-logo" alt={"Google logo"} src={"google-logo-2.png"} />
+            <div className="continue-with-google">
+              <span className="text-wrapper-6">Continue with Google</span>
+            </div>
+          </div>
+          <p  className="by-clicking-agree-and-join-to-the-linkedin-user-agreement-privacy-policy-and-cookie-policy">
+            <span className="text-wrapper-6">By clicking Agree and join to the LinkedIn User Agreement, Privacy Policy, and Cookie Policy.
+            </span>
+        <span className="text-wrapper-8">
+        User <br />
+        Agreement Privacy Policy and Cookie Policy
+      </span>
+      </p>
+      <p className="already-on-linkedin-sign-in">
+        <span className="text-wrapper-6">Already on LinkedIn?</span>
+        <span className="text-wrapper-8">Sign in</span>
+      </p>
+      
+      <div className="or-line">
+        <img className="line" alt="Line" src="line-2-1.png" />
+        <div className="or">
+          <span className="text-wrapper-9">or</span>
+        </div>
+        <img className="line-2" alt="Line" src="line-2-1.png" />
+      </div>
+      </div>
+      <p className="1looking-to-create-a-page-for-a-business-get-help">
+        <span className="text-wrapper-6">Looking to create a page for a business?</span>
+        <span className="text-wrapper-8">Get help</span>
+      </p>
+      </div>
+    </div>
+       );
+}
